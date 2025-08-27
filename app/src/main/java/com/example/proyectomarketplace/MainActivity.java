458994ffc5b324f38c0afa56c0de4b7dfe8d2b89
miton56 <simonbarrosquepasa@gmail.com>
2025-08-27 15:59:19 -0400

    algo

--- app/src/main/java/com/example/proyectomarketplace/MainActivity.java
+++ app/src/main/java/com/example/proyectomarketplace/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.proyectomarketplace;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void mostrarMensaje(View view) {
+        Toast.makeText(this, "holaaaaa", Toast.LENGTH_LONG).show();
     }
 }
